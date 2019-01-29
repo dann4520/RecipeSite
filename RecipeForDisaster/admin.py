851b49fac08d74aabe5dde88d4c56d375edf705d
dann4520 <stabdan@gmail.com>
2019-01-29 12:22:21 -0600

    Housekeeping. Removed commented out code that is no longer used. Made a few PEP 8 corrections.

--- RecipeForDisaster/admin.py
+++ RecipeForDisaster/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 
 from .models import Recipe, Ingredient
-'''
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
 
-'''
+
 class ChoiceInline(admin.TabularInline):
     model = Ingredient
     extra = 3
 
+
 class RecipeAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ('name',)
+
+
 admin.site.register(Recipe, RecipeAdmin)
--- RecipeForDisaster/apps.py
+++ RecipeForDisaster/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class RecipefordisasterConfig(AppConfig):
+class RecipeForDisasterConfig(AppConfig):
     name = 'RecipeForDisaster'
